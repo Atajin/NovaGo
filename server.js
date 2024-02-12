251ff518e1383a70601316970ecb8b9b10f38141
Billy-Le-Chu <99690278+Billy-Le-Chu@users.noreply.github.com>
2024-02-11 19:29:38 -0500

    ajouter nav

--- server.js
+++ server.js
@@ -44,7 +44,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.render('pages/index', {
+    //res.render('pages/index', {
+        res.render('pages/principal', {
         // variables
     });
 });
