10c60fb4ef1ca524e6dc8cb75e941a63efa6da20
Billy Le Chu <99690278+Billy-Le-Chu@users.noreply.github.com>
2024-02-12 11:44:53 -0500

    image + fix resize ecran

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ app.set("view engine", "ejs");
 */
 app.use("/js", express.static(__dirname + "/node_modules/bootstrap/dist/js"));
 app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
+app.use(express.static(__dirname + "/static/images"));
 
 /*
     Permettre l'utilisation de body lors des POST request
