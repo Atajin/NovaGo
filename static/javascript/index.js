1669dc661eadc66dc256fbd58a6118586d250e32
HauntedBaguette <95427341+HauntedBaguette@users.noreply.github.com>
2024-04-03 10:58:39 -0400

    Séparation des scripts pour chaque page

---
+++ static/javascript/index.js
@@ -0,0 +1,12 @@
+document.getElementById('aller-simple').addEventListener('change', function () {
+    document.getElementById('date-retour-col').style.display = 'none';
+    document.getElementById('date-aller-col').classList.remove('col-6');
+    document.getElementById('date-aller-col').classList.add('col');
+    document.getElementById('date-retour').value = '';
+});
+
+document.getElementById('aller-retour').addEventListener('change', function () {
+    document.getElementById('date-retour-col').style.display = 'block';
+    document.getElementById('date-aller-col').classList.remove('col');
+    document.getElementById('date-aller-col').classList.add('col-6');
+});
